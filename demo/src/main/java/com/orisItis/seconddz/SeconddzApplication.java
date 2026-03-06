0fa529da2edf6eff532e6005d425e223fd816a2d
Kkkaterina <eicolonel@icloud.com>
2026-03-06 19:57:11 +0300

    Второе дз

---
+++ demo/src/main/java/com/orisItis/seconddz/SeconddzApplication.java
@@ -0,0 +1,13 @@
+package com.orisItis.seconddz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeconddzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeconddzApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/orisItis/seconddz/SeconddzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orisItis.seconddz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeconddzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
